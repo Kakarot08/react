6d04c9bed988901256ca0ded9bbfe3e2759e0a50
Abhishek Dhillon <abhishek.dhillon@paxcel.net>
2020-07-30 15:07:26 +0530

    Hosting

---
+++ Y/precache-manifest.14aae31f2910c300aff568488677e4cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2f1c506dd518eeea6b27bd46c5c869",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da82a01884ff713f76fb",
+    "url": "/static/css/main.11f7f152.chunk.css"
+  },
+  {
+    "revision": "2616e1e196fceb50955f",
+    "url": "/static/js/2.be1592a7.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.be1592a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da82a01884ff713f76fb",
+    "url": "/static/js/main.b40e6501.chunk.js"
+  },
+  {
+    "revision": "8625f9c0a6c681457eb3",
+    "url": "/static/js/runtime-main.892ad9de.js"
+  }
+]);
---
+++ Y/precache-manifest.aef28f5e70e27c6ee4039b5bc4466f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135f5d85cfb38d6c1e5b8f5cb3d02300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ca2246f9d61a2d23669",
+    "url": "/static/css/main.11f7f152.chunk.css"
+  },
+  {
+    "revision": "2616e1e196fceb50955f",
+    "url": "/static/js/2.be1592a7.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.be1592a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca2246f9d61a2d23669",
+    "url": "/static/js/main.faa8bc64.chunk.js"
+  },
+  {
+    "revision": "8625f9c0a6c681457eb3",
+    "url": "/static/js/runtime-main.892ad9de.js"
+  }
+]);
